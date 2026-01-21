81652cfa3954304573a61be793e7ac791549f0cb
flatton <tomoki.fujihara@abejainc.com>
2026-01-21 12:05:52 +0900

    add test for vllm openai api compatible server

---
+++ packages/llm_inference/src/llm_inference/vllm_openai_api_compatible.py
@@ -0,0 +1,39 @@
+from logging import Logger
+from typing import Any
+
+from openai import OpenAI
+from transformers import AutoTokenizer
+
+from .data import Conversation, Prompt
+from .openai_api import OpenAIGenerator
+
+
+class VLLMOpenAICompatibleGenerator(OpenAIGenerator):
+    def __init__(
+        self,
+        *,
+        client: OpenAI,
+        tokenizer: AutoTokenizer,
+        model_name: str,
+        max_context_length: int,
+        max_output_tokens: int,
+        logger: Logger,
+    ) -> None:
+        super().__init__(
+            client=client,
+            model_name=model_name,
+            max_context_length=max_context_length,
+            max_output_tokens=max_output_tokens,
+            logger=logger,
+        )
+        self.tokenizer = tokenizer
+
+    def _call_token_count_api(self, input: Prompt | Conversation, **kwargs: Any) -> int:
+        if isinstance(input, Conversation):
+            return len(
+                self.tokenizer.apply_chat_template(
+                    input.prompt, tokenize=True, **kwargs
+                )
+            )
+        else:
+            return len(self.tokenizer.encode(input.prompt, add_special_tokens=False))
